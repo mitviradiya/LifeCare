7535cdbd318d166b681cee7e85b936ceaa1a69b3
mitviradiya <mitviradiya@gmail.com>
2017-04-03 12:21:37 +0530

    Add files via upload

---
+++ customer/connection.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$con=mysql_connect("localhost","root","");
+mysql_select_db("lifecare",$con);
+if(isset($_SESSION['username']))
+{	
+}
+else
+{
+	echo "<script>document.location='../index.php'</script>";
+}
+?>
